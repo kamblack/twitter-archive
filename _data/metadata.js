a70d65bdd65085fc9ff93e17e4563e6e97642e96
kamblack <me@kam.black>
2023-05-22 10:39:44 -0600

    Archive built

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,9 +1,9 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "HarlanTheFox", // No leading @ here
+	homeLabel: "Studio Yip!",
+	homeUrl: "https://www.studioyip.com/",
 };
 
-data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
+data.avatar = `img/harlan-icon-200.png`;
 
 module.exports = data;
